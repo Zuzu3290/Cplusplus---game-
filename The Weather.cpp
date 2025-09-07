0d4ac118324983a63be6942b2881ec9a19deaf16
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:37:39 +0200

    Create The Weather.cpp

---
+++ The Weather.cpp
@@ -0,0 +1,2 @@
+// trying to interact with data using the C++ language. This would help me in tackling logging data, data file handling and simple summarizing data. 
+//  interesting Recording and viewing daily weather readings. Very basic. lettsss dive in.
