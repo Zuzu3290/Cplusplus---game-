50cf2d7f38b5b99a8f16741b1f485149c4f5b514
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:47:45 +0200

    Create Gradebook.cpp

---
+++ Gradebook.cpp
@@ -0,0 +1,7 @@
+ // A simple OOB project to imply the application of every day student [erformance profile. 
+// Objective: Record students’ grades and compute averages.
+// Tasks:
+// Classes: Student, Course, Gradebook.
+// Store grades in file.
+// Calculate GPA and ranking.
+// Structure: OOP for relationships + file handling
