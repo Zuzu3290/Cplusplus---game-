0d2e0cc17185b8a9c3c74ddaf3215f81e7d594e2
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:54:56 +0200

    Create BAS.cpp

---
+++ BAS.cpp
@@ -0,0 +1,7 @@
+// Bank Account Simulator
+// Teaches: Transactions, balances, error handling. Closer to real-life financial applications.
+// Objective: Simulate deposits, withdrawals, and balance checks.
+// Tasks:// Classes: Account, Transaction.
+// Save accounts to file.
+// Handle multiple users.
+// Structure: Banking operations loop + persistence.
