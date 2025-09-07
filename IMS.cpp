bdbf88a4c3d288a439b7eb5ba3b71bb45eebb5fe
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:51:00 +0200

    Create IMS.cpp

---
+++ IMS.cpp
@@ -0,0 +1,7 @@
+// Inventory Management System
+// Teaches: Product stock tracking, update/delete operations.  Realistic small business scenario. 
+// Objective: Manage products in a small store.Tasks:
+// Classes: Product, Inventory.
+// Add, delete, and update stock.
+// Generate summary reports.
+// Structure: File I/O with menu-driven interface.
