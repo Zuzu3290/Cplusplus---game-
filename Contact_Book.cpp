0a3d6b1407d3d2cf468814927785c6f71aeb3989
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:31:55 +0200

    Create Contact_Book.cpp

---
+++ Contact_Book.cpp
@@ -0,0 +1,3 @@
+// This replication of a contact book using C++, however the focus in on enhancing and applying OOb theoritical knowledge on a project. 
+// This would require Classes and a file management type structure, but I look forward to working and applying CRUD systems. 
+// Oh Oh this project would be a great learning lesson applying low level langauge to understand the operations of Git on this level. Interesting. while the Structure is to use CRUD with a menu -driven system. 
