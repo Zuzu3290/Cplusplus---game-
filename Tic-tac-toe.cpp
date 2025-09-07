7aa7c89da2834d72fbabc3f2097456ca6db2c333
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 13:24:55 +0200

    Create Tic-tac-toe.cpp

---
+++ Tic-tac-toe.cpp
@@ -0,0 +1,3 @@
+// the game is to tackle the concepts of Game loop, 2D array and win conditions. APplying simple and basic concpets of C++. A fundamental starter project. 
+// Objective: Classic game implementation. An epiphany moment this project is somewhat also capable to replicating my undersatdning of an environment and navigating it. So, I guess implying the use of coordiantes is the technique. 
+// Thinking about it I could enhance the appliocation by implementiang OOB. So far the strcuture is simple : (Input --> update --> display) --> acknowledge the winner
