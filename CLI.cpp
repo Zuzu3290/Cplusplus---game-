802515e1474f4f2c3448e87702d8725c19cad6c2
Muhammad Zuhair <zuhairmuhammad16@gmail.com>
2025-09-07 14:24:36 +0200

    Create CLI.cpp

---
+++ CLI.cpp
@@ -0,0 +1,8 @@
+// Simple Text Editor (CLI)
+// Teaches: String handling, search/replace, line storage.
+// Involves deeper file manipulation.
+// Objective: Basic editing like add, search, and delete lines.Tasks:
+// Classes: Document, Editor.
+// File read/write.
+// Implement search and replace.
+// Structure: Document object + operations.
